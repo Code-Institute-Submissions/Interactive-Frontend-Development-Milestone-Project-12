06c0f9544a314e208feabbfb41516ac6395c47b9
dalecars <carson.dale@gmail.com>
2020-04-29 11:44:02 +0000

    button displayed on all pages with js to scroll to top when user scrolls down the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,29 @@ function changeIcon(icon) {
     icon.classList.toggle("change");
 }
 
+/* functions to display back to top button when user scrolls down  */
+
+//Get the button:
+topBtn = document.getElementById("topBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
 $(document).ready(function(){
 
     $("#robusta_coffee").click(function () {
