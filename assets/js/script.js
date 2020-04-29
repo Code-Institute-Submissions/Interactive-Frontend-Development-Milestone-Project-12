45f9a3e1762e3dd9f7339dce3a8515006f2e1dc5
dalecars <carson.dale@gmail.com>
2020-04-29 10:07:00 +0000

    coffee bean images added to coffee-belt.html with js to change img src on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,34 @@ function toggleSidePanel() {
 function changeIcon(icon) {
     icon.classList.toggle("change");
 }
+
+$(document).ready(function(){
+
+    $("#robusta_coffee").click(function () {
+        if($(this).attr("src") === "assets/images/robusta_coffee.jpg"){
+            $(this).fadeOut(300, function() {
+                $(this).attr("src", "assets/images/robusta_bean.jpg");
+                document.getElementById("robusta_zoom").src = "assets/images/zoom_out.png";
+            }).fadeIn(300);
+        }  else {
+             $(this).fadeOut(300, function() {
+                $(this).attr("src", "assets/images/robusta_coffee.jpg");
+                document.getElementById("robusta_zoom").src = "assets/images/zoom_in.png";
+             }).fadeIn(300);
+        }        
+    });
+
+   $("#arabica_coffee").click(function () {
+        if($(this).attr("src") === "assets/images/arabica_coffee.jpg"){
+            $(this).fadeOut(300, function() {
+                $(this).attr("src", "assets/images/arabica_bean.jpg");
+                document.getElementById("arabica_zoom").src = "assets/images/zoom_out.png";
+            }).fadeIn(300);
+        }  else {
+             $(this).fadeOut(300, function() {
+                $(this).attr("src", "assets/images/arabica_coffee.jpg");
+                document.getElementById("arabica_zoom").src = "assets/images/zoom_in.png";
+             }).fadeIn(300);
+        }        
+    });
+});
--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,26 +1,31 @@
-/* slide show functions */
+// var to set the slide index, initally set to display first slide
 var slideIndex = 1;
+
+// call function to show the slide
 showSlides(slideIndex);
 
+// function to increment/decrement the slide index when next/prev is clicked and call the showSlide function
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// function to set the slide index to the corresponding dot clicked and call showSlide function
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-    var i;
+//function to display the slide for the slideindex passed
+function showSlides(slideNumber) {
+    var count;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+    if (slideNumber > slides.length) {slideIndex = 1}    
+    if (slideNumber < 1) {slideIndex = slides.length}
+    for (count = 0; count < slides.length; count++) {
+        slides[count].style.display = "none";  
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" dotActive", "");
+    for (count = 0; count < dots.length; count++) {
+        dots[count].className = dots[count].className.replace(" dotActive", "");
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " dotActive";
